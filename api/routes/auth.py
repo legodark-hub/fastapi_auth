5db9b99b674fe53f63d6528099226d79d78d1a68
aidarminyazev <legodark@mail.ru>
2025-08-09 17:30:26 +0500

    add user routes

---
+++ api/routes/auth.py
@@ -0,0 +1,35 @@
+from datetime import timedelta
+
+from fastapi import APIRouter, Depends, HTTPException
+from fastapi.security import OAuth2PasswordRequestForm
+from sqlalchemy.ext.asyncio import AsyncSession
+
+from api.services.auth import authenticate_user, register_new_user
+from database.connection import get_db
+from schemas.token import Token
+from schemas.user import User, UserCreate
+from utils.security import create_access_token
+from config import settings
+
+router = APIRouter()
+
+
+@router.post("/register", response_model=User)
+async def register(user: UserCreate, db: AsyncSession = Depends(get_db)):
+    return await register_new_user(db=db, user=user)
+
+
+@router.post("/token", response_model=Token)
+async def login_for_access_token(form_data: OAuth2PasswordRequestForm = Depends(), db: AsyncSession = Depends(get_db)):
+    user = await authenticate_user(db, email=form_data.username, password=form_data.password)
+    if not user:
+        raise HTTPException(
+            status_code=401,
+            detail="Incorrect username or password",
+            headers={"WWW-Authenticate": "Bearer"},
+        )
+    access_token_expires = timedelta(minutes=settings.ACCESS_TOKEN_EXPIRE_MINUTES)
+    access_token = create_access_token(
+        data={"sub": user.email}, expires_delta=access_token_expires
+    )
+    return {"access_token": access_token, "token_type": "bearer"}
